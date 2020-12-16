f6496d1c8f24ab95aa0749dae495773a7c5159d2
ArmeriaWang <armeriaw@outlook.com>
2020-12-16 21:25:14 +0800

    Update Main.java

--- src/main/java/Main.java
+++ src/main/java/Main.java
@@ -18,18 +18,18 @@ public class Main {
     public static void init() {
         fileList.add(new File(dataFilePath + "eil51.tsp"));
         fileList.add(new File(dataFilePath + "eil76.tsp"));
-//        fileList.add(new File(dataFilePath + "eil101.tsp"));
-//        fileList.add(new File(dataFilePath + "st70.tsp"));
-//        fileList.add(new File(dataFilePath + "kroA100.tsp"));
-//        fileList.add(new File(dataFilePath + "kroC100.tsp"));
-//        fileList.add(new File(dataFilePath + "kroD100.tsp"));
-//        fileList.add(new File(dataFilePath + "lin105.tsp"));
-//        fileList.add(new File(dataFilePath + "pcb442.tsp"));
-//        fileList.add(new File(dataFilePath + "pr2392.tsp"));
+        fileList.add(new File(dataFilePath + "eil101.tsp"));
+        fileList.add(new File(dataFilePath + "st70.tsp"));
+        fileList.add(new File(dataFilePath + "kroA100.tsp"));
+        fileList.add(new File(dataFilePath + "kroC100.tsp"));
+        fileList.add(new File(dataFilePath + "kroD100.tsp"));
+        fileList.add(new File(dataFilePath + "lin105.tsp"));
+        fileList.add(new File(dataFilePath + "pcb442.tsp"));
+        fileList.add(new File(dataFilePath + "pr2392.tsp"));
         populationSizeList.add(10);
         populationSizeList.add(20);
-//        populationSizeList.add(50);
-//        populationSizeList.add(100);
+        populationSizeList.add(50);
+        populationSizeList.add(100);
     }
 
     public static void runExp() throws IOException {
